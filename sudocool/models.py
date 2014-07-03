21c0be3f97fe41bf7483c8d811e01e2022d66ca1
Robert Gwynn-Jones <gwynnr@gmail.com>
2014-07-03 23:26:29 +1000

    Use auto_now_add instead of manually setting created_date Remove unneeded import
    Thanks Mike!

--- sudocool/models.py
+++ sudocool/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-from django.utils import timezone
 
 class SudocoolBoardManager(models.Manager):
     def create_sudocoolboard(self, sudocoolData):
-        sudocoolBoard = self.create(created_date = timezone.now(), sudocoolData = sudocoolData)
+        sudocoolBoard = self.create(sudocoolData = sudocoolData)
         sudocoolBoard.save()
         return sudocoolBoard
 
 
 class SudocoolBoard(models.Model):
-    created_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
     sudocoolData = models.CharField(max_length=161)
 
     def __unicode__(self):
