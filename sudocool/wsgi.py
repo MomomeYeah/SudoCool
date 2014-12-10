0b6599730575f876b362614c2e38d61c67b7f1a8
Robert Gwynn-Jones <gwynnr@gmail.com>
2014-12-11 07:13:48 +1100

    Update to wsgi file for Heroku

--- sudocool/wsgi.py
+++ sudocool/wsgi.py
@@ -7,12 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
-from django.core.wsgi import get_wsgi_application
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-if os.environ.get('DEPLOY_ENVIRONMENT') == 'heroku':
-    from dj_static import Cling
-    application = Cling(get_wsgi_application())
-else:
-    application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
