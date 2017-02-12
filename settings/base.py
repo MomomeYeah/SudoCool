c9cb7bc1bcb5b447020edfbd91ca841c3dd15450
Rob Gwynn-Jones <gwynnr@gmail.com>
2017-02-12 13:37:45 +1100

    Add STATIC_ROOT for Heroku

--- settings/base.py
+++ settings/base.py
@@ -92,4 +92,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
